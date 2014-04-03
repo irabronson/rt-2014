6a6d95ac27d2759709ca26573c85c65e03daf533
Saki Sato <sakiisato@gmail.com>
2014-04-03 16:43:46 -0400

    changed taxonomy to artist instead of artist_category to fix urls

--- wp-content/plugins/custom-post-type-artist.php
+++ wp-content/plugins/custom-post-type-artist.php
@@ -93,7 +93,7 @@ function custom_taxonomies_artist() {
 		'labels' => $labels,
 		'hierarchical' => true,
 	);
-	register_taxonomy( 'artist_category', 'artist', $args );
+	register_taxonomy( 'artists', 'artist', $args );
 }
 add_action( 'init', 'custom_taxonomies_artist', 0 );
 
@@ -105,3 +105,53 @@ add_action( 'init', 'custom_taxonomies_artist', 0 );
 //     return $query;
 // }
 // add_action( 'pre_get_posts', 'add_artist_posts_to_query' );
+
+// // for permalink rewriting
+// add_filter('rewrite_rules_array', 'mmp_rewrite_rules');
+// function mmp_rewrite_rules($rules) {
+//     $newRules  = array();
+//     $newRules['basename/(.+)/(.+)/(.+)/(.+)/?$'] = 'index.php?custom_post_type_name=$matches[4]'; // my custom structure will always have the post name as the 5th uri segment
+//     $newRules['basename/(.+)/?$']                = 'index.php?taxonomy_name=$matches[1]'; 
+// 
+//     return array_merge($newRules, $rules);
+// }
+// 
+// function filter_post_type_link($link, $post)
+// {
+//     if ($post->post_type != 'custom_post_type_name')
+//         return $link;
+// 
+//     if ($cats = get_the_terms($post->ID, 'taxonomy_name'))
+//     {
+//         $link = str_replace('%taxonomy_name%', get_taxonomy_parents(array_pop($cats)->term_id, 'taxonomy_name', false, '/', true), $link); // see custom function defined below
+//     }
+//     return $link;
+// }
+// add_filter('post_type_link', 'filter_post_type_link', 10, 2);
+// 
+// // my own function to do what get_category_parents does for other taxonomies
+// function get_taxonomy_parents($id, $taxonomy, $link = false, $separator = '/', $nicename = false, $visited = array()) {    
+//     $chain = '';   
+//     $parent = &get_term($id, $taxonomy);
+// 
+//     if (is_wp_error($parent)) {
+//         return $parent;
+//     }
+// 
+//     if ($nicename)    
+//         $name = $parent -> slug;        
+// else    
+//         $name = $parent -> name;
+// 
+//     if ($parent -> parent && ($parent -> parent != $parent -> term_id) && !in_array($parent -> parent, $visited)) {    
+//         $visited[] = $parent -> parent;    
+//         $chain .= get_taxonomy_parents($parent -> parent, $taxonomy, $link, $separator, $nicename, $visited);
+// 
+//     }
+// 
+//     if ($link) {
+//         // nothing, can't get this working :(
+//     } else    
+//         $chain .= $name . $separator;    
+//     return $chain;    
+// }
