b24d1434a9c7aecb945599b4ead922ae597acde5
Saki Sato <sakiisato@gmail.com>
2014-04-23 11:57:44 -0400

    moved most functions out of doc.ready, only left DOM interaction there, also added conditionals to test for existing elements before binding

--- wp-content/themes/razorandtie/js/main.js
+++ wp-content/themes/razorandtie/js/main.js
@@ -1,8 +1,110 @@
 jQuery(function($) {
     
+    // Toggle Artist List
+    // ******************
+    // Between image and text
+    var toggleDisplay = function (e) {
+        
+        $('#toggle-display a').unbind('click', toggleDisplay);
+        
+        e.preventDefault();
+        
+        var activeClass = $(this).attr('id');
+        
+        if( $(this).hasClass('active') === false ) {
+            $('#toggle-display a.active').removeClass('active');
+            $(this).addClass('active');
+            
+            $('.artists').removeClass('image-display text-display').addClass(activeClass);
+        }
+        
+        $('#toggle-display a').bind('click', toggleDisplay);
+    };
+    
+    // Artist Category Filtering
+    // *************************
+    // Currently only appears on the homepage
+    var filterArtists = function (e) {
+        e.preventDefault();
+        
+        $('ul#filters a').off( 'click', filterArtists );
+        
+        var filterVal = $(this).attr('data-filter');
+        var itemsLength = $('.artists .artist:visible').length;
+        
+        $('.artists .artist:visible').each(function(i) {
+            $(this).fadeOut(30, function(){
+                if(itemsLength == ++i) {
+                    showArtists(filterVal);
+                }
+            });
+        });
+        
+        // Adds/removes 'active' class to selected item
+        // Adds the 'active' class to the first anchor
+        // $('ul#filters a').first().addClass("active");
+        $('ul#filters .active').removeClass('active');
+        $(this).addClass('active');
+        
+        $('ul#filters a').on( 'click', filterArtists );
+    };
+    
+    // Shows results for
+    // Artists Category Filter
+    var showArtists = function(filterVal) {
+        if(filterVal == 'all') {
+            $('.artists .artist').fadeIn(200);
+        } else {
+            $('.artists .artist').each(function() {
+                if($(this).attr('data-filter') === filterVal ) {
+                    $(this).fadeIn(200);
+                }
+            });
+        }
+    };
+    
+    // AJAX page loading
+    // *****************
+    // For News Posts on homepage
+    var getNewsPage = function(e) {
+        
+        e.preventDefault();
+        
+        $(this).off('click', getNewsPage);
+        
+        var link = $(this).attr('href');
+        $('#news').html('Loading...');
+        
+        $('#news').load(link + ' .news-wrapper', function( response, status, xhr ) {
+            bindNewsPagination();
+        });
+        
+    };
+    
+    // News pagination binding function
+    var bindNewsPagination = function () {
+        $('#pagination a').on('click', getNewsPage);
+    };
+    
+    // Select Option Navigation
+    // ************************
+    // Appears on Artist Detail page as supplementary nav
+    var selectOptNav = function () {
+        
+        if ($(this).val()!='') {
+            $('#artist-detail-nav').off('change', selectOptNav );
+            window.location.href=$(this).val();
+            $('#artist-detail-nav').html('<option>Loading...</option>');
+        }
+    };
+    
+    
+    // DOM events after doc.ready
+    // **************************
     $(document).ready(function () {
         
-        // BROWSER QUERY
+        // Browser Queries
+        //****************
         // Add class to <html> based on browser
         if (navigator.userAgent.indexOf('Mac OS X') != -1) {
             // Mac
@@ -23,34 +125,40 @@ jQuery(function($) {
             if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { $('html').addClass('safari-Win'); }
         }
         
-        // Bind Artist Detail Select to Navigation
-        $('#artist-detail-nav').on('change', function () {
-            if ($(this).val()!='') {
-              window.location.href=$(this).val();
-            }
-        });
-        
         // Toggle Artist List
-        // Between image and text
-        var toggleDisplay = function (e) {
-            
-            $('#toggle-display a').unbind('click', toggleDisplay);
-            
-            e.preventDefault();
-            
-            var activeClass = $(this).attr('id');
-            
-            if( $(this).hasClass('active') === false ) {
-                $('#toggle-display a.active').removeClass('active');
-                $(this).addClass('active');
-                
-                $('.artists').removeClass('image-display text-display').addClass(activeClass);
-            }
-            
+        // ******************
+        if( $('#toggle-display').length > 0 ) {
             $('#toggle-display a').bind('click', toggleDisplay);
-        };
+        }
         
-        $('#toggle-display a').bind('click', toggleDisplay);
+        // Artist Category Filtering
+        // *************************
+        if( $('#filters').length > 0 ) {
+            $('ul#filters a').on( 'click', filterArtists );
+            
+            // Preselecting a filter option
+            $('#filters a[data-filter="razor-tie"]').trigger('click');
+            
+            // Sets timeout and fades in content
+            // Best for 'FlashOfUnfilteredContent'
+            setTimeout(function (){
+                $('.primary').fadeTo( 150 , 1 );
+            }, 150);
+        }
+        
+        // AJAX page loading
+        // *****************
+        // Bind news pagination
+        // Where #news exists
+        if( $('#news').length > 0 ) {
+            bindNewsPagination();
+        }
+        
+        // Select Option Navigation
+        // ************************
+        if( $('#artist-detail-nav').length > 0 ) {
+            $('#artist-detail-nav').on('change', selectOptNav );
+        }
         
     }); // End doc.ready
 }); // End jQuery
