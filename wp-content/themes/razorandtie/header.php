5f328948c999dd79290f6c5fe746fcb086f33c1d
Saki Sato <sakiisato@gmail.com>
2014-04-23 11:58:03 -0400

    removed html5.js for iE, ira said its included in modernizr

--- wp-content/themes/razorandtie/header.php
+++ wp-content/themes/razorandtie/header.php
@@ -37,10 +37,6 @@
         <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/style.css" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         
-        <!--[if lt IE 9]>
-            <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-        <![endif]-->
-        
         <?php wp_enqueue_scripts(); ?>
         
         <?php wp_head(); ?>
