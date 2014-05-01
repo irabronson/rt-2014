175111fa2941dd3a9cf1cd9a823bf1edde0ed7e2
Ira Bronson <irabronson@gmail.com>
2014-05-01 17:51:01 -0400

    Added container section-inner.

--- wp-content/themes/razorandtie/content-page.php
+++ wp-content/themes/razorandtie/content-page.php
@@ -11,7 +11,8 @@
 
 <!-- Default Page Template -->
 <section class="primary">
+  <section class="section-inner">
     <h2><?php wp_title(''); ?></h2>
-    
     <div class="text"><?php echo $post->post_content; ?></div>
+  </section><!-- .section-inner -->    
 </section>
